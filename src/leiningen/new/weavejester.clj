81617beaa8324d1f9e3312fd07f790447882786e
James Reeves <jreeves@weavejester.com>
2016-04-07 18:05:56 +0100

    Add template files from default Lein template

--- src/leiningen/new/weavejester.clj
+++ src/leiningen/new/weavejester.clj
@@ -1,24 +1,39 @@
 (ns leiningen.new.weavejester
-  (:require [leiningen.new.templates :refer [renderer name-to-path ->files]]
+  (:require [leiningen.new.templates :as tmpl]
             [leiningen.core.main :as main]))
 
-(def render (renderer "weavejester"))
+(def render (tmpl/renderer "weavejester"))
 
 (defn template-data [name]
-  {:name name
-   :sanitized (name-to-path name)})
+  (let [main-ns (tmpl/multi-segment (tmpl/sanitize-ns name))]
+    {:raw-name    name
+     :name        (tmpl/project-name name)
+     :namespace   main-ns
+     :nested-dirs (tmpl/name-to-path main-ns)
+     :year        (tmpl/year)
+     :date        (tmpl/date)}))
 
 (defn clojure [name]
   (let [data (template-data name)]
     (main/info "Generating new Clojure library project:" name)
-    (->files data
-             ["src/{{sanitized}}/foo.clj" (render "foo.clj" data)])))
+    (tmpl/->files data
+                  ["project.clj" (render "project.clj" data)]
+                  ["README.md"   (render "README.md" data)]
+                  [".gitignore"  (render "gitignore" data)]
+                  ["src/{{nested-dirs}}.clj"       (render "core.clj" data)]
+                  ["test/{{nested-dirs}}_test.clj" (render "test.clj" data)]
+                  "resources")))
 
 (defn clojurescript [name]
   (let [data (template-data name)]
     (main/info "Generating new ClojureScript library project:" name)
-    (->files data
-             ["src/{{sanitized}}/foo.clj" (render "foo.clj" data)])))
+    (tmpl/->files data
+                  ["project.clj" (render "project.clj" data)]
+                  ["README.md"   (render "README.md" data)]
+                  [".gitignore"  (render "gitignore" data)]
+                  ["src/{{nested-dirs}}.clj"       (render "core.clj" data)]
+                  ["test/{{nested-dirs}}_test.clj" (render "test.clj" data)]
+                  "resources")))
 
 (defn weavejester
   "Generate a new Clojure or ClojureScript project."
@@ -29,3 +44,4 @@
      "clojure"       (clojure name)
      "clojurescript" (clojurescript name)
      (main/abort "No such project type:" type))))
+  

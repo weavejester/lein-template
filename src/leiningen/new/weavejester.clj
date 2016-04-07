ab298e0caad565dc7d47a08b6f116bf84fe2068f
James Reeves <jreeves@weavejester.com>
2016-04-07 17:11:59 +0100

    Add templates types for Clojure and ClojureScript

--- src/leiningen/new/weavejester.clj
+++ src/leiningen/new/weavejester.clj
@@ -4,11 +4,28 @@
 
 (def render (renderer "weavejester"))
 
-(defn weavejester
-  "FIXME: write documentation"
-  [name]
-  (let [data {:name name
-              :sanitized (name-to-path name)}]
-    (main/info "Generating fresh 'lein new' weavejester project.")
+(defn template-data [name]
+  {:name name
+   :sanitized (name-to-path name)})
+
+(defn clojure [name]
+  (let [data (template-data name)]
+    (main/info "Generating new Clojure library project:" name)
     (->files data
              ["src/{{sanitized}}/foo.clj" (render "foo.clj" data)])))
+
+(defn clojurescript [name]
+  (let [data (template-data name)]
+    (main/info "Generating new ClojureScript library project:" name)
+    (->files data
+             ["src/{{sanitized}}/foo.clj" (render "foo.clj" data)])))
+
+(defn weavejester
+  "Generate a new Clojure or ClojureScript project."
+  ([name]
+   (weavejester name "clojure"))
+  ([name type]
+   (case type
+     "clojure"       (clojure name)
+     "clojurescript" (clojurescript name)
+     (main/abort "No such project type:" type))))

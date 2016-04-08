2282022edf8bdc19b3a6f322ec57ba6f425a953a
James Reeves <jreeves@weavejester.com>
2016-04-08 15:23:08 +0100

    Add description and URL to project file

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject weavejester/lein-template "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Leiningen templates for my projects"
+  :url "https://github.com/weavejester/lein-template"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
